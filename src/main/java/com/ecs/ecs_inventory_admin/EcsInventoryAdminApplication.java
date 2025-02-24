3f78b0c809ff4ec506cb65b0d67fbbcb520b6084
= <=>
2025-02-24 15:10:51 -0500

    initial push from local

---
+++ src/main/java/com/ecs/ecs_inventory_admin/EcsInventoryAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_inventory_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsInventoryAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsInventoryAdminApplication.class, args);
+	}
+
+}
